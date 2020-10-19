2dc7aedfe054ba9fef7ebe7e36f860390660ab00
Brian Hung <brianhung@berkeley.edu>
2020-10-19 00:58:25 -0700

    allow codeblocks to be escaped with gapcursors

--- src/gapcursor.js
+++ src/gapcursor.js
@@ -104,7 +104,7 @@ function closedBefore($pos) {
     if (index == 0) continue
     // See if the node before (or its first ancestor) is closed
     for (let before = $pos.node(d).child(index - 1);; before = before.lastChild) {
-      if ((before.childCount == 0 && !before.inlineContent) || before.isAtom || before.type.spec.isolating) return true
+      if ((before.childCount == 0 && !before.inlineContent) || before.isAtom || before.type.spec.isolating || before.type.spec.code) return true
       if (before.inlineContent) return false
     }
   }
@@ -117,7 +117,7 @@ function closedAfter($pos) {
     let index = $pos.indexAfter(d), parent = $pos.node(d)
     if (index == parent.childCount) continue
     for (let after = parent.child(index);; after = after.firstChild) {
-      if ((after.childCount == 0 && !after.inlineContent) || after.isAtom || after.type.spec.isolating) return true
+      if ((after.childCount == 0 && !after.inlineContent) || after.isAtom || after.type.spec.isolating || after.type.spec.code) return true
       if (after.inlineContent) return false
     }
   }
